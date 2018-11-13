9893b24990f7683fab2f07eb30faab28832c5c8b
Madi <madelyn.e.helton@gmail.com>
2018-11-13 03:05:42 -0600

    adding pictures

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,60 @@
 //Make an object of all the crystals
 
+let crystal = [
+  {
+    name: "A",
+    image: ''
+  },
+  {
+    name: "B",
+    image: ''
+  },
+  {
+    name: "C",
+    image: ''
+  },
+  {
+    name: "D",
+    image: ''
+  },
+];
+
 //make the randomNum var
+
+//Make a on click action for all 4 crystals make a function
+
+// each crystal are a different random amonut that adds to total player a
+
+
+
+
+
+// $("#btn").on("click", function(){
+
+//   $("#name").
+  
+// })
+
+// $("#btn").click(function() {
+//   $("#name").animate({
+//     opacity: 0.25,
+//     left: "+=50",
+//     height: "toggle"
+//   }, 5000, function() {
+//     // Animation complete.
+//   });
+// });
+
+// $("#btn").on("click", function(){
+//   $("#score").hide();
+//   start();
+// });
+// let fullName  ; 
+
+//  $("#submit").on("click", function() {
+//    event.preventDefault();
+// let firstName = $("#firstName").val();
+// let lastName = $("#lastName").val();
+// fullName = firstName + lastName;
+// });
+
